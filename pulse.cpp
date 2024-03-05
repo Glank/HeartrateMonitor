2f2def9e0224fb7c2d19c7f2bede802417cea330
Ernest Kirstein <glank314@gmail.com>
2024-03-05 02:05:19 -0500

    Switching to a linked-list implementation.

--- pulse.cpp
+++ pulse.cpp
@@ -39,6 +39,7 @@ bool PulseTrackerInternals::detect_peak(long now) {
   if (!maximum)
     return false;
 
+  /*
   Peak& peak = peaks.push_back();
   peak.t = now-(PULSE_SLOPE_WINDOW-max_i-1)*1000/PULSE_SAMPLE_RATE;
   peak.amp = max_amp;
@@ -47,172 +48,56 @@ bool PulseTrackerInternals::detect_peak(long now) {
   peak.std = -1;
   peak.val = '_';
   peak.d = -1;
+  */
   return true;
 }
-void PulseTrackerInternals::update_widths() {
-  widths_head = peaks.size()-2;
-  if(peaks.size() < 3) {
-    // we need at least 3 peaks to calculate the width
-    peaks.back().w = 0;
-    return;
+void WidthCalculatingStream::push(std::shared_ptr<Peak> p) {
+  if (size==0) {
+    tail = p;
+    head = p;
+    size = 1;
+  } else {
+    head->next = p;
+    head = p;
+    size++;
   }
-  peaks[widths_head].w = peaks[widths_head+1].t-peaks[widths_head-1].t;
-}
-bool PulseTrackerInternals::update_stats() {
-  // "validation window" = the time > PULSE_VALIDATION_WINDOW_MS around the peak at stats_head
-  // In practice, the validation window should go from [stats_tail to widths_head-1] with
-  // stats_head approximately in the middle.
-  // This function works as follows:
-  //   First, we check to see if we have enouogh lead time in the buffer, or if we need to wait for more pulses to come in.
-  //   Then, we snug up the tail so that the validation window is as small as possible while still longer than specified
-  //   Then we calculate the average and standard deviation within the window and save it to the stats_head peak.
-  // Warning: stats_head and stats_tail are smart indices that are automatically decremented every time peaks' head moves.
-  if (stats_head<0)
-    stats_head = 0;
-  if (stats_tail<0)
-    stats_tail = 0;
-  // if we can't fit the validation window around the stats head and have only peaks with measured widths,
-  // we can't update
-  if(stats_head >= peaks.size()
-    || widths_head-1 < 0
-    || (peaks[widths_head-1].t-peaks[stats_head].t)<PULSE_VALIDATION_WINDOW_MS/2) {
-    return false;
-  }
-  
-  // move the stats_tail forward in time if there is slack in the validation window
-  while(stats_tail < stats_head && (peaks[stats_head].t-peaks[stats_tail+1].t)>PULSE_VALIDATION_WINDOW_MS/2) {
-    stats_tail++;
+  if (size>3) {
+    tail = tail->next;
+    size--;
   }
 
-  // If the stats tail is too close, we can't calculate the stats.
-  // This should only happen while we don't have enough peaks, or stats_head has fallen behind
-  // because of a flood of high frequency peaks.
-  if (peaks[stats_head].t-peaks[stats_tail].t < PULSE_VALIDATION_WINDOW_MS/2) {
-    #ifdef PULSE_DEBUG
-    if (peaks.full() && stats_tail > 0)
-      Serial.println("Error: Stats tail moved too close!");
-    #endif
-    stats_head++;
-    return false;
-  }
-
-  float avg = peaks.calc_smart_sum(widths_sum, stats_tail, widths_head)/(widths_head-stats_tail);
-  float avg2 = peaks.calc_smart_sum(widths2_sum, stats_tail, widths_head)/(widths_head-stats_tail);
-  peaks[stats_head].avg = avg;
-  peaks[stats_head].std = sqrt(avg2-avg*avg);
-  stats_head++;
-  return true;
-}
-bool PulseTrackerInternals::inspect_pulse() {
-  if (inspection_head < 0)
-    inspection_head = 0;
-  Peak& p = peaks[inspection_head];
-  if (p.avg == -1) {
-    if (inspection_head >= stats_head-1) {
-      // caught up to stat's head
-      return false;
-    }
-    inspection_head++;
-    return true;
-  }
-  if(p.std != 0 && (p.w-p.avg)/p.std < -1 && p.w/p.avg < 0.7)
-    p.val = '?';
-  else
-    p.val = 'v';
-
-  inspection_head++;
-  return true;
-}
-bool PulseTrackerInternals::resolve_questionable() {
-  if (resolution_head < 0)
-    resolution_head = 0;
-  if (resolution_tail < 0)
-    resolution_tail = 0;
-  if (resolution_head >= peaks.size()) {
-    // edge case in the very begining of processing
-    return false;
-  }
-  if (peaks[resolution_head].val == '_') {
-    if (resolution_head >= inspection_head-1) {
-      // caught up to inspeciton head
-      return false;
-    }
-    resolution_head++;
-    resolution_tail=resolution_head;
-    return true;
-  }
-  if (peaks[resolution_head].val == 'v' && resolution_tail == resolution_head) {
-    // nothing to resolve, continue
-    resolution_head++;
-    resolution_tail++;
-    return true;
-  }
-  if (peaks[resolution_head].val == '?') {
-    // leave the tail at it's current position to capture this questionable group
-    resolution_head++;
-    return true;
-  }
-  // at this point we can assume we're at the end of a questionable group
   #ifdef PULSE_DEBUG
-  if (peaks[resolution_head].val != 'v' || resolution_head <= resolution_tail) {
-    char l[128];
-    sprintf(l, "Error: Invalid stream state in resolve_questionable! head: %d, tail: %d, val_at_head: %c",
-      resolution_head, resolution_tail, peaks[resolution_head].val);
-    Serial.println(l);
-  }
+  if (size > 3)
+    Serial.println("Error: unexpected stream state, size>3");
   #endif
 
-  int num_questionable = resolution_head-resolution_tail;
+  if(size != 3)
+    return;
+  auto mid = tail->next;
+  mid->w = head->t - tail->t;
 
-  // if we just have an isolated questionable pulse, just mark it as false and move on
-  if (num_questionable == 1) {
-    peaks[resolution_tail].val = 'f';
-    resolution_head++;
-    resolution_tail = resolution_head;
-    return true;
-  }
-  
-  // otherwise, calculate the average even and odd amplitudes
-  // and mark the set with the smaller average as false
-  float avg_e = 0;
-  float avg_o = 0;
-  for (int i = 0; i < num_questionable; i++){
-    if(i%2==0)
-      avg_e += peaks[resolution_tail+i].amp;
-    else
-      avg_o += peaks[resolution_tail+i].amp;
-  }
-  avg_e /= (int)((num_questionable+1)/2);
-  avg_o /= (int)(num_questionable/2);
-  int valid_parity = avg_e < avg_o;
-  for (int i = 0; i < num_questionable; i++) {
-    peaks[resolution_tail+i].val = (i%2 == valid_parity) ? 'v' : 'f';
-  }
-  
-  resolution_head++;
-  resolution_tail = resolution_head;
-  return true;
+  if(next_stream==nullptr)
+    return;
+  next_stream->push(mid);
 }
-bool PulseTrackerInternals::update_deltas() {
-  if (deltas_head < 0)
-    deltas_head = 0;
-  // waiting for more.
-  if (deltas_head <= peaks.size() || deltas_head+1 >= resolution_tail)
-    return false;
-  // TODO
-  return false;
+std::shared_ptr<Peak> WidthCalculatingStream::pop() {
+  if (size==0)
+    return nullptr;
+  size--;
+  auto tmp = tail;
+  tail = tail->next;
+  if(size<=1)
+    head = tail;
+  return tmp;
 }
-void PulseTrackerInternals::update_hr() {
-  // TODO
+void WidthCalculatingStream::set_next(PeakProcessingStream* n) {
+  next_stream = n;
 }
 void PulseTrackerInternals::push(int pulse_signal, long time) {
   pulse_signals.push_back() = pulse_signal;
   if(!detect_peak(time))
     return;
-  update_widths();
-  while(update_stats());
-  while(inspect_pulse());
-  while(resolve_questionable());
+  //TODO
 }
 void PulseTrackerInternals::get_heartrate(HeartRate* out) const {
   out->time = -1;
--- pulse.h
+++ pulse.h
@@ -26,154 +26,26 @@ struct HeartRate {
 };
 
 struct Peak {
-  long t; //time
-  int amp; //amplitude
+  long t = -1; //time
+  int amp = -1; //amplitude
   // width (next peak's time - previous peak's time) and
   // average and standard deviation of the width in relation to nearby (within ~PULSE_VALIDATION_WINDOW_MS/2) peaks.
   // if havent yet been calculated, they will be equal to -1
-  float w, avg, std;
-  char val; //validation state:
+  float w = -1;
+  float avg = -1;
+  float std = -1;
+  char val = '_'; //validation state:
     // '_' = unvalidated
     // '?' = potentially a false pulse
     // 'f' = definitely a false pulse
     // 'v' = valid pulse
-  float d; // delta (time till the next valid pulse)
-};
+  float d = -1; // delta (time till the next valid pulse)
 
-template <typename T> class RingBuffer;
-template <typename T> class RBStream;
+  std::shared_ptr<Peak> next = nullptr;
 
-template <typename T>
-class SyncedIndex {
-  private:
-    int i;
-    const int buf_cap;
-    std::function<void()> before_increment = nullptr;
-    SyncedIndex& operator=(SyncedIndex& other) {
-      #ifdef PULSE_DEBUG
-      if (other.buf_cap != buf_cap)
-        Serial.println("Error: Trying to copy-assign mismatched SyncedIndex buf_caps!");
-      #endif
-      i = other.i;
-    }
-  public:
-    SyncedIndex(int i, int buf_cap) : buf_cap(buf_cap) {
-      i = i;
-    }
-    void set_before_increment(std::function<void()> f) {
-      before_increment = f;
-    }
-    SyncedIndex& operator++() {
-      if (before_increment != nullptr)
-        before_increment();
-      i = (i+1)%buf_cap;
-      return *this;
-    }
-    bool operator==(const SyncedIndex& other) {
-      #ifdef PULSE_DEBUG
-      if (other.buf_cap != buf_cap)
-        Serial.println("Error: Trying to compare mismatched SyncedIndex buf_caps!");
-      #endif
-      return i == other.i;
-    }
-    int operator-(const SyncedIndex& other) const {
-      #ifdef PULSE_DEBUG
-      if (other.buf_cap != buf_cap)
-        Serial.println("Error: Trying to subtract mismatched SyncedIndex buf_caps!");
-      #endif
-      return (buf_cap+i-other.i)%buf_cap;
-    }
-  friend RingBuffer<T>;
-  friend RBStream<T>;
-};
-
-template <typename T>
-class RBStream {
-  protected:
-    std::vector<std::unique_ptr<SyncedIndex<T>>> heads;
-    RingBuffer<T>* const parent;
-  public:
-    RBStream(RingBuffer<T>* parent, int n_heads) : parent(parent) {
-      heads.reserve(n_heads);
-      for(int i = 0; i < n_heads; i++)
-        heads.push_back(std::make_unique<SyncedIndex<T>>(parent->h, parent->cap));
-    }
-    void inc(int h) {
-      ++(*(heads[h]));
-    }
-    SyncedIndex<T>& head(int h) {
-      return *(heads[h]);
-    }
-    T& at(int h) {
-      return (*parent)[head(h)];
-    }
-    T& at(SyncedIndex<T>& i) {
-      return (*parent)[i];
-    }
-};
-
-// A 3 headed stream that tries to position 3 heads (tail, mid, and front)
-// to satisfy the constraints specified in front_is_balanced(), mid_is_balanced(),
-// and back_is_balanced().
-// Implementation Note:
-// Once balanced, the mid should *stay* balanced, because skipping the mid head forward
-// is treated as a missed result, skipping the output stream forward to compensate.
-// Implementations should never manually increment the heads, that's all handled by the
-// automatic balancing logic and the write() function.
-template <typename T>
-class BalancedStream : public RBStream<T> {
-  private:
-    BalancedStream<T>* output = nullptr;
-  public:
-    virtual bool front_is_blanced() { return true; }
-    virtual bool mid_is_balanced() { return true; }
-    virtual bool back_is_balanced() { return true; }
-    virtual void after_push_when_balanced() = 0;
-    virtual void before_back_increment() {};
-    SyncedIndex<T>& back() { return this->head(0); }
-    SyncedIndex<T>& mid() { return this->head(1); }
-    SyncedIndex<T>& front() { return this->head(2); }
-    BalancedStream(RingBuffer<T>* parent) : RBStream<T>(parent, 3) {
-      // ensure that the mid head stays at or ahead of the back head
-      // and the front head stays ahead of the mid head
-      back().set_before_increment([&]{
-        this->before_back_increment();
-        if(mid() == back())
-          ++mid();
-      });
-      mid().set_before_increment([&]{
-        if(front() == mid())
-          ++front();
-      });
-    }
-    void set_output(BalancedStream<T>* out) {
-      output = out;
-    }
-  protected:
-    // moves the mid forward and pushes to output.
-    void write() {
-      ++mid();
-      output->push();
-    }
-  public:
-    void skip() {
-      ++front();
-      ++mid();
-      ++back();
-    }
-    void push() {
-      ++front();
-      while(back() != mid() && !this->back_is_balanced()) {
-        ++back();
-      }
-      while(mid() != front() && !this->mid_is_balanced()) {
-        ++mid();
-        output->skip();
-      }
-      if (!this->back_is_balanced() || !this->mid_is_balanced() || !this->front_is_balanced())
-        return;
-      this->after_push_when_balanced();
-    }
+  ~Peak() {
+    next = nullptr;
+  };
 };
 
 template <typename T>
@@ -183,14 +55,6 @@ class RingBuffer {
     int len; //length
     int cap; // capacity
     std::unique_ptr<T[]> buffer;
-    std::vector<std::unique_ptr<RBStream<T>>> streams;
-  protected:
-    virtual void on_advance(T& drop) {
-      for(auto& s : streams) {
-        if((s->head(0).i+1)%cap == h)
-          s->inc(0);
-      }
-    }
   public:
     RingBuffer(int capacity) : buffer(new T[capacity]) {
       h = 0;
@@ -201,15 +65,10 @@ class RingBuffer {
     T& operator[]( const int i ) const {
       return buffer[(h+i)%cap];
     }
-    T& operator[]( const SyncedIndex<T>& idx ) const {
-      return buffer[idx.i];
-    }
     T& push_back() {
       T& ret = buffer[(h+len)%cap];
-      if (len == cap) {
+      if (len == cap)
         h++;
-        on_advance(ret);
-      }
       else
         len++;
       return ret;
@@ -217,134 +76,87 @@ class RingBuffer {
     T& back() {
       return buffer[(h+len-1)%cap];
     }
+    T pop_front() {
+      int temp = h;
+      h = (h+1)%cap;
+      len--;
+      return buffer[temp];
+    }
     int size() const { return len; }
     int capacity() const { return cap; }
     bool full() const { return len==cap; }
-    RBStream<T>* new_stream(int heads) {
-      streams.push_back(std::make_unique<RBStream<T>>(this, heads));
-      return streams.back().get();
-    }
-    int relative(const SyncedIndex<T>& idx) const {
-      // h+r = i (mod cap)
-      // r = i-h (mod cap)
-      return (cap+idx.i-h)%cap;
-    }
-  friend RBStream<T>;
+    bool empty() const { return len==0; }
 };
 
-template <typename T, typename S>
-class SumStream : BalancedStream<T> {
-  S sum;
-  int age;
-  public:
-    virtual S value(T& elem) = 0;
-    SumStream(RingBuffer<T>* parent) : BalancedStream<T>(parent), sum(0) {
-      this->front().set_before_increment([&]{
-        sum = sum + this->value(this->at(this->front()));
-        age++;
-      });
-    }
-    void before_back_increment() override {
-      sum = sum - this->value(this->at(this->back()));
-      age++;
-    }
-};
-
-class PeakBuffer : public RingBuffer<Peak> {
+template <typename T, unsigned int N>
+class MemStack {
   private:
-    struct SumWindow {
-      std::function<float(Peak&)> value;
-      // inclusive
-      int head, tail;
-      int lifetime;
-      float sum;
+    T mem[N];
+    T *free[N];
+    int n_free;
+    struct MSDeleter {
+      MemStack* const p;
+      MSDeleter(MemStack* parent) : p(parent) {}
+      void operator()(T* t) {
+        t->~T();
+        p->free[p->n_free] = t;
+        p->n_free++;
+      }
     };
-    std::vector<int*> smart_indexes;
-    std::vector<std::unique_ptr<SumWindow>> sum_windows;
-    const std::function<int()> rand_int;
-  protected:
-    void on_advance(Peak& drop) {
-      for (int* si : smart_indexes) {
-        (*si)--;
-      }
-      for(auto& swp : sum_windows) {
-        auto& sw = *swp;
-        if (sw.tail < 0 && sw.head >= 0) {
-          sw.sum -= sw.value(drop);
-          sw.lifetime--;
-        }
-      }
-    }
+  std::function<std::shared_ptr<T>()> steal = nullptr;
   public:
-    PeakBuffer(int capacity=PULSE_PEAKS_LEN, std::function<int()> rand_generator=rand)
-      : RingBuffer<Peak>(capacity), rand_int(rand_generator) {}
-    // TODO: move these function defs to pulse.cpp
-    void add_smart_index(int* index) {
-      smart_indexes.push_back(index);
+    MemStack() {
+      for(int i = 0; i < N; i++)
+        free[i] = &(mem[i]);
+      n_free = N;
     }
-    int register_smart_sum(std::function<float(Peak&)> value) {
-      std::unique_ptr<SumWindow> sw(new SumWindow());
-      sw->head = -1;
-      sw->tail = -1;
-      sw->lifetime = 0;
-      sw->sum = 0;
-      sw->value = value;
-      add_smart_index(&sw->head);
-      add_smart_index(&sw->tail);
-      sum_windows.push_back(std::move(sw));
-      return sum_windows.size()-1;
+    void set_steal(std::function<std::shared_ptr<T>()> f) {
+      steal = f;
     }
-    // key is the return from register_smart_average
-    // start is inclusive
-    // end is exclusive
-    float calc_smart_sum(int key, int start, int end) {
-      if (start == end)
-        return 0;
-      SumWindow& sw = *sum_windows[key];
-      // recalculate when the sum gets too old to account for accumulated floating point errors.
-      if (sw.lifetime <= 0) {
-        // use a pseudo-random lifetime to prevent having to
-        // recalculate all of these at the same time.
-        sw.lifetime = rand_int()%PULSE_MAX_SMART_SUM_AGE;
-        sw.head = end-1;
-        sw.tail = start;
-        sw.sum = 0;
-        for (int i = start; i < end; i++) {
-          sw.sum += sw.value((*this)[i]);
-        }
-        return sw.sum;
+    std::shared_ptr<T> make() {
+      if (n_free == 0) {
+        #ifdef PULSE_DEBUG
+        if(steal == nullptr)
+          Serial.println("Error! No thief allocator in MemStack.");
+        #endif
+        std::shared_ptr<T> stolen = steal();
+        stolen->~T();
+        #ifdef PULSE_DEBUG
+        if(stolen.use_count() != 1)
+          Serial.println("Error! Memory could not be free'd to make new in MemStack.");
+        #endif
       }
-      // match the tail
-      while(sw.tail < start) {
-        if(sw.tail>=0) {
-          sw.sum -= sw.value((*this)[sw.tail]);
-          sw.lifetime--;
-        }
-        sw.tail++;
-      }
-      while(sw.tail > start) {
-        sw.tail--;
-        sw.sum += sw.value((*this)[sw.tail]);
-        sw.lifetime--;
-      }
-      // match the head
-      int new_head = end-1;
-      while(sw.head > new_head) {
-        sw.sum -= sw.value((*this)[sw.head]);
-        sw.lifetime--;
-        sw.head--;
-      }
-      while(sw.head < new_head) {
-        sw.head++;
-        sw.sum += sw.value((*this)[sw.head]);
-        sw.lifetime--;
-      }
-      return sw.sum;
+      if (n_free == 0)
+        ESP.restart();
+      return std::shared_ptr<T>(free[--n_free], MSDeleter(this));
     }
+    int num_free() {
+      return n_free;
+    }
+};
+
+class PeakProcessingStream {
+  public:
+    virtual void push(std::shared_ptr<Peak> p) = 0;
+    virtual std::shared_ptr<Peak> pop() = 0;
+};
+
+class WidthCalculatingStream : PeakProcessingStream {
+  private:
+    int size = 0;
+    std::shared_ptr<Peak> tail = nullptr;
+    std::shared_ptr<Peak> head = nullptr;
+    PeakProcessingStream* next_stream = nullptr;
+  public:
+    WidthCalculatingStream() {}
+    void push(std::shared_ptr<Peak> p) override;
+    std::shared_ptr<Peak> pop() override;
+    void set_next(PeakProcessingStream* next);
 };
 
 class PulseTrackerInternals {
   public:
+    MemStack<Peak, PULSE_PEAKS_LEN> peak_mem;
     // record samples for long enough to calculate the slope accurately
     RingBuffer<int> pulse_signals;
     // calculates the slope and max of the current pulse_signals
@@ -355,29 +167,6 @@ class PulseTrackerInternals {
     // increasing to decreasing, and if so, push a peak on the stack
     bool detect_peak(long now);
 
-    PeakBuffer peaks;
-    RingBuffer<HeartRate> hr_swap_buf;
-    // pointers to various bits of work that need to be done on Peaks
-    int widths_head = 0; // updated in update_widths
-    int stats_head = 0; // updated in peaks.on_advance & update_stats
-    int stats_tail = 0; // updated in peaks.on_advance & update_stats
-    int inspection_head = 0; // updated in peaks.on_advance & inspection_pulse
-    int resolution_head = 0; // updated in peaks.on_advance & resolve_questionable
-    int resolution_tail = 0; // updated in peaks.on_advance & resolve_questionable
-    int deltas_head = 0; // updated in peaks.on_advance & update_deltas
-    // keys for smart sum in peaks
-    int widths_sum, widths2_sum, delta_count, delta_sum, delta2_sum;
-    // four resonably complex clean-up steps that are split up because
-    // they operate at different points on the peak buffer, and should be
-    // separately tested
-    // The ones with boolean returns should be repeated until they return false;
-    void update_widths();
-    bool update_stats();
-    bool inspect_pulse();
-    bool resolve_questionable();
-    bool update_deltas();
-    void update_hr();
-
     // Fast func to push a signal onto the buffer. Not safe to be interrupted.
     // Also calls all of the above update functions so that get_heartrate has
     // as little work to do as possible.
@@ -385,19 +174,7 @@ class PulseTrackerInternals {
     // Safe to be interrupted
     void get_heartrate(HeartRate* out) const;
 
-    PulseTrackerInternals() : pulse_signals(PULSE_SLOPE_WINDOW), hr_swap_buf(2) {
-      peaks.add_smart_index(&stats_head);
-      peaks.add_smart_index(&stats_tail);
-      peaks.add_smart_index(&inspection_head);
-      peaks.add_smart_index(&resolution_head);
-      peaks.add_smart_index(&resolution_tail);
-      peaks.add_smart_index(&deltas_head);
-      widths_sum = peaks.register_smart_sum([](Peak& p){return p.w;});
-      widths2_sum = peaks.register_smart_sum([](Peak& p){return p.w*p.w;});
-      delta_count = peaks.register_smart_sum([](Peak& p){return p.d<0?0:1;});
-      delta_sum = peaks.register_smart_sum([](Peak& p){return p.d<0?0:p.d;});
-      delta2_sum = peaks.register_smart_sum([](Peak& p){return p.d<0?0:p.d*p.d;});
-    }
+    PulseTrackerInternals() : pulse_signals(PULSE_SLOPE_WINDOW) {}
 };
 
 // public wrapper of PulseTrackerInternals
